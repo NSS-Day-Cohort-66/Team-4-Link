574c747bcef55eca1ef076b56d2d4e1622c88914
Derek Malone <laxmalone@gmail.com>
2023-07-13 10:54:02 -0500

    implemented nested for loop and discussed ternaries and switch case

--- main.js
+++ main.js
@@ -16,34 +16,77 @@ const meals = [];
   possibility of using switch/case or ternaries for practice instead of if statements
   3 levels of cook functions: beginnerCook for 2 ingredients, intermediateCook for 3 ingredients, and advancedCook for 4 ingredients
 */
-const cook = (firstIngredient, secondIngredient) => {
-  if (
-    firstIngredient === "Hylian Rice" &&
-    secondIngredient === "Big Hearty Truffle"
-  ) {
-    meals.push("Mushroom Rice Balls");
-  } else if (
-    firstIngredient === "Hateno Cheese" &&
-    secondIngredient === "Bird Egg"
-  ) {
-    meals.push("Cheesy Omelette");
-  } else if (
-    firstIngredient === "Tabantha Wheat" &&
-    secondIngredient === "Hateno Cheese"
-  ) {
-    meals.push("Cheesy Hylian Pizza");
-  } else if (
-    firstIngredient === "Raw Prime Meat" &&
-    secondIngredient === "Hylian Rice"
-  ) {
-    meals.push("Prime Meat and Rice Bowl");
-  }
+
+// ternary: (something > nothing ? "do something" : "do nothing")
+// const cook = (firstIngredient, secondIngredient) => {
+//   if (
+//     firstIngredient === "Hylian Rice" &&
+//     secondIngredient === "Big Hearty Truffle"
+//   ) {
+//     meals.push("Mushroom Rice Balls");
+//   } else if (
+//     firstIngredient === "Hateno Cheese" &&
+//     secondIngredient === "Bird Egg"
+//   ) {
+//     meals.push("Cheesy Omelette");
+//   } else if (
+//     firstIngredient === "Tabantha Wheat" &&
+//     secondIngredient === "Hateno Cheese"
+//   ) {
+//     meals.push("Cheesy Hylian Pizza");
+//   } else if (
+//     firstIngredient === "Raw Prime Meat" &&
+//     secondIngredient === "Hylian Rice"
+//   ) {
+//     meals.push("Prime Meat and Rice Bowl");
+//   }
+//   // else {
+//   //   meals.push("Unknown recipe");
+//   // }
+// };
+
+// HACK: Need to look at how to implement switch case.
+// const cook = (firstIngredient, secondIngredient) => {
+//   switch ((firstIngredient, secondIngredient)) {
+//     case firstIngredient === "Hylian Rice" &&
+//       secondIngredient === "Big Hearty Truffle":
+//       meals.push("Mushroom Rice Balls");
+//       break;
+//     case firstIngredient === "Hateno Cheese" && secondIngredient === "Bird Egg":
+//       meals.push("Cheesy Omelette");
+//       break;
+//   }
+  // if (
+  //   firstIngredient === "Hylian Rice" &&
+  //   secondIngredient === "Big Hearty Truffle"
+  // ) {
+  //   meals.push("Mushroom Rice Balls");
+  // } else if (
+  //   firstIngredient === "Hateno Cheese" &&
+  //   secondIngredient === "Bird Egg"
+  // ) {
+  //   meals.push("Cheesy Omelette");
+  // } else if (
+  //   firstIngredient === "Tabantha Wheat" &&
+  //   secondIngredient === "Hateno Cheese"
+  // ) {
+  //   meals.push("Cheesy Hylian Pizza");
+  // } else if (
+  //   firstIngredient === "Raw Prime Meat" &&
+  //   secondIngredient === "Hylian Rice"
+  // ) {
+  //   meals.push("Prime Meat and Rice Bowl");
+  // }
+  // else {
+  //   meals.push("Unknown recipe");
+  // }
 };
 
-// HACK: We could possibly iterate over each item in the array with a loop.
-cook(materials[2], materials[4]);
-cook(materials[0], materials[1]);
-cook(materials[3], materials[0]);
+for (let i = 0; i < materials.length; i++) {
+  for (let j = 0; j < materials.length; j++) {
+    cook(materials[i], materials[j]);
+  }
+}
 
 console.log("MEALS:");
 console.log("---------------");
